f6b127f9c460f048c62730bee79b8ecb53eeeb45
edgarlcs <92342090+edgarlcs@users.noreply.github.com>
2022-11-09 16:50:54 -0700

    next auth test(not working)

--- rifadisimo/pages/api/auth/[...nextauth].js
+++ rifadisimo/pages/api/auth/[...nextauth].js
@@ -7,7 +7,30 @@ export const authOptions = {
       clientId: process.env.FB_ID,
       clientSecret: process.env.FB_SECRET,
     }),
-    // ...add more providers here
   ],
+  session: {
+    jwt: true,
+  },
+  callbacks: {
+    async session({ session, token, user }) {
+      session.jwt = token.jwt;
+      session.id = token.id;
+      return session;
+    },
+    async jwt({ token, user, account }) {
+      if (user) {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/auth/google/callback?access_token=${account.access_token}`
+        );
+        const data = await response.json();
+        token.jwt = data.jwt;
+        token.id = data.user.id;
+      }
+      return token;
+    },
+  },
 };
-export default NextAuth(authOptions);
+
+const Auth = (req, res) => NextAuth(req, res, authOptions);
+
+export default Auth;
